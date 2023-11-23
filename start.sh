3706f782312688e4c4a7c2206d657dcd4e0aba0d
Masonville17 <masonville17@gmail.com>
2023-11-23 00:31:18 -0700

    adding vpn information to 10m informatic interval

--- start.sh
+++ start.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 VPN_INTERFACE="tun0"
+OPENVPN_CONFIGS_ZIP_URL="https://configs.ipvanish.com/configs/configs.zip"
 
-wget https://configs.ipvanish.com/configs/configs.zip
-unzip configs.zip
-rm configs.zip
+# Local ports to exclude from VPN
+echo "Adding iptable rules"
+iptables -A OUTPUT -o tun0 -p tcp --dport 8080 -j DROP
+iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+
+# downloading and configuring openvpn files / connection
+
+# download and inflate, remove zipfile and exclude countries
+wget $OPENVPN_CONFIGS_ZIP_URL && unzip *.zip && rm *.zip
 
 readarray -t countries < exclude_countries
-
 for country in "${countries[@]}"
 do
     echo "Removing files with pattern *-$country-*"
     rm -rf *-"$country"-*
 done
 
+# select random ovpn file and establish connection
 OVPN_FILE=$(find . -name "*.ovpn" | shuf -n 1)
-echo "Today, we'll connect to $OVPN_FILE"
-
-#remove no longer supported openvpn parameter.
+echo "Establishing Connection with $OVPN_FILE"
 sed -i '/keysize/d' "${OVPN_FILE}"
+openvpn --config "${OVPN_FILE}" --auth-user-pass pass & 
+vpn_pid=$!
 
-openvpn --config "${OVPN_FILE}" --auth-user-pass pass &
-
-sleep 5
-echo "vpn should be connected by now."
-ip a
-sleep 15
-# Local ports to exclude from VPN
-iptables -A OUTPUT -o tun0 -p tcp --dport 8080 -j DROP
-iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+# Wait for VPN to establish connection and check if connection is established
+sleep 45
+if ip a show tun0 up > /dev/null 2>&1; then
+    echo "VPN is connected."
+else
+    echo "VPN connection failed."
+    exit 1
+fi
 
 echo "starting configuring qbnox"
 
-mkdir ./qbnox && cd ./qbnox
-
 while true; do
+    vpn_infos=$(ps -f -p $vpn_pid)
     qbnoxinfos=$(qbittorrent-nox)
     ipinfos=$(ip a)
-    echo "qbnox info: $qbnoxinfos... ipinfos: $ipinfos... Sleeping 10m..."
+    echo ""vpn infos: PID: $vpn_pid, $vpn_infos... qbnox info: $qbnoxinfos... ipinfos: $ipinfos... Sleeping 10m..."
     sleep 600
 done
 wait
